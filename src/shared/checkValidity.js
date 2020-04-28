0e9ea58925c3a9c7d38498605f2d667cce863f33
Timucin Acar <timucin.acar@cologne-intelligence.de>
2020-04-28 13:35:44 +0200

    fix bugs in field validation

--- src/shared/checkValidity.js
+++ src/shared/checkValidity.js
@@ -4,26 +4,26 @@ const checkValidity = (value, rules) => {
     return errorArray;
   }
 
-  if (rules.required) {
-    value.trim() !== "" && errorArray.push("Field is empty!");
-  }
-
-  if (rules.minLength) {
-    value.length > rules.minLength && errorArray.push("Value too short");
-  }
-
-  if (rules.maxLength) {
-    value.length < rules.maxLength && errorArray.push("Value too long");
+  if (rules.isRequired) {
+    value.trim() === "" && errorArray.push("Field is empty!");
   }
 
   if (rules.isEmail) {
-    const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-    pattern.test(value) && errorArray.push("Not a valid email!");
+    const pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    !(pattern.test(value)) && errorArray.push("Not a valid email!");
   }
 
   if (rules.isPassword) {
     const pattern = /^(?=.{8,})(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$/;
-     !(pattern.test(value)) && errorArray.push("Not a valid password! Valid Special characters are: '@ # $ % ^ & + ='");
+    !(pattern.test(value)) && errorArray.push("Not a valid password! Valid Special characters are: '@ # $ % ^ & + ='");
+  }
+
+  if (rules.minLength) {
+    value.length < rules.minLength && errorArray.push("Value too short");
+  }
+
+  if (rules.maxLength) {
+    value.length > rules.maxLength && errorArray.push("Value too long");
   }
 
   return errorArray;
