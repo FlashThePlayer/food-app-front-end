0b31eb364736a0e34e465be0b9a55606d43b1b88
Timucin Acar <timucin.acar@cologne-intelligence.de>
2020-05-29 17:10:38 +0200

    include authentication token to header

--- src/GraphQl/ApolloClient.js
+++ src/GraphQl/ApolloClient.js
@@ -1,7 +1,28 @@
-import ApolloClient from "apollo-boost";
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { setContext } from 'apollo-link-context';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { useSelector } from "react-redux";
+
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3000/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("token")
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+});
 
 const defaultClient = new ApolloClient({
-  uri: "http://localhost:3000/graphql",
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
 });
 
 export default defaultClient;
