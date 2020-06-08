b372af0b3bb749bc7f05c454040f38cb3844f299
Timucin Acar <timucin.acar@cologne-intelligence.de>
2020-06-08 23:02:00 +0200

    enables query to getFoods for user

--- src/GraphQl/Schema/Schema.js
+++ src/GraphQl/Schema/Schema.js
@@ -9,16 +9,31 @@ export const createUserSchema = gql`
   }
 `;
 
-export const loginUserSchema = gql `
-    mutation loginUser($userInput: loginUserInputData){
-        loginUser(userInput: $userInput)
-    }
+export const loginUserSchema = gql`
+  mutation loginUser($userInput: loginUserInputData) {
+    loginUser(userInput: $userInput)
+  }
 `;
 
-export const createFoodSchema = gql `
-    mutation createFood($foodInput: FoodInputData){
-        createFood(foodInput: $foodInput) {
-            name
-        }
+export const createFoodSchema = gql`
+  mutation createFood($foodInput: FoodInputData) {
+    createFood(foodInput: $foodInput) {
+      name
     }
+  }
+`;
+
+export const getFoodsSchema = gql`
+  query getFoods($page: Int) {
+    getFoods(page: $page) {
+      foods {
+        name
+        link
+        favorite
+        rating
+        difficulty
+      }
+      totalPages
+    }
+  }
 `;
