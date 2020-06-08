08108d4ce8702d57400e0ea6216cc53999eb974b
Timucin Acar <timucin.acar@cologne-intelligence.de>
2020-06-08 23:00:58 +0200

    add FoodItem component to display saved recopies in the database

---
+++ src/UI/FoodItem/FoodItem.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import classes from "./FoodItem.module.css"
+
+const FoodItem = props => {
+    return (
+        <div className={classes.FoodItem}>
+            <p>{props.name}</p>
+            <p>{props.link}</p>
+            <p>{props.favorite}</p>
+            <p>{props.rating}</p>
+            <p>{props.difficulty}</p>
+        </div>
+    )
+}
+
+export default FoodItem;
