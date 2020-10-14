120cf9b35a5c3120ca8cf93e8689e53db1d9f089
Timucin Acar <timucin.acar@cologne-intelligence.de>
2020-10-14 16:07:36 +0200

    add new components QueryComponent and FoodSelection to GetFoods
    just as stated in the last commit, added the components to reduce code duplication

--- src/Pages/GetFoods/GetFoods.js
+++ src/Pages/GetFoods/GetFoods.js
@@ -2,16 +2,10 @@ import React, { useEffect, useState } from "react";
 import { useLazyQuery } from "@apollo/client";
 import { getFoodsSchema } from "../../GraphQl/Schema/Schema";
 import classes from "../GetFoods/GetFoods.module.css";
-import Spinner from "../../UI/Spinner/Spinner";
-import FoodItem from "../../UI/FoodItem/FoodItem";
-import Pageination from "../../UI/Pageniation/Pageination";
-import { useForm } from "react-hook-form";
-import createInput, { createSelect } from "../../Shared/createFormFields";
-import SubmitButton from "../../UI/Button/SubmitButton/SubmitButton";
-import {createQueryFormFromSchema} from "../../Shared/createQueryFormFromSchema";
+import FoodSelection from "../../UI/FoodSelection/FoodSelection";
+import QueryComponent from "../../UI/QueryComponent/QueryComponent";
 
-const GetFoods = (props) => {
-  const { register, handleSubmit, errors } = useForm({ mode: "onBlur" });
+const GetFoods = () => {
   const [getFoods, { loading, data }] = useLazyQuery(getFoodsSchema);
 
   const [page, setPage] = useState(1);
@@ -28,15 +22,6 @@ const GetFoods = (props) => {
       setTotalPages(data.getFoods.totalPages);
     }
   }, [setFoodArray, setTotalPages, data]);
-
-  const queryFormSchema = {
-    name: createInput("", "foodName", "Name of the food", "", {}),
-    favorite: createInput("checkbox", "favorite", "false", "", {}),
-    stars: createInput("number", "rating", "", "1", {}),
-    difficulty: createSelect("difficulty", ["easy", "normal", "hard"], {}),
-  };
-
-  let queryForm = createQueryFormFromSchema(queryFormSchema, errors, register)
 
   const onSubmitHandler = ({ foodName, favorite, rating, difficulty }) => {
     getFoods({
@@ -59,58 +44,20 @@ const GetFoods = (props) => {
     setPage((prevState) => prevState - 1);
   };
 
-  let foodList;
-  let pages;
-
-  if (loading) {
-    foodList = (
-      <div className={classes.Spinner}>
-        <Spinner />
-      </div>
-    );
-  } else {
-    foodList = foodArray.map((food, index) => {
-      return (
-        <FoodItem
-          id={index}
-          name={food.name}
-          link={food.link}
-          favorite={food.favorite}
-          rating={food.rating}
-          difficulty={food.difficulty}
-        />
-      );
-    });
-    pages = (
-      <Pageination
-        currentPage={page}
-        maxPages={totalPages}
-        prevPage={prevPageHandler}
-        nextPage={nextPageHandler}
-      />
-    );
-  }
-
-  if (foodList.length === 0) {
-    foodList = (
-      <div className={classes.NoFoodBanner}>
-        <p>Create some food already!</p>
-      </div>
-    );
-    pages = null;
-  }
-
   return (
     <React.Fragment>
       <div className={classes.QuerySearch}>
-        <form className={classes.QueryForm} onSubmit={handleSubmit(onSubmitHandler)}>
-          {queryForm}
-          <SubmitButton>Search</SubmitButton>
-        </form>
+        <QueryComponent submitHandler={onSubmitHandler}/>
       </div>
       <div className={classes.Content}>
-        {foodList}
-        {pages}
+        <FoodSelection
+            loading={loading}
+            foods={foodArray}
+            page={page}
+            totalPages={totalPages}
+            prevPageHandler={prevPageHandler}
+            nextPageHandler={nextPageHandler}
+        />
       </div>
     </React.Fragment>
   );
