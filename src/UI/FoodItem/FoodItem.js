a59fee2914dc5b6f650e6c87102b4d799ef50fd6
Timucin Acar <timucin.acar@cologne-intelligence.de>
2020-12-20 17:19:02 +0100

    prettify FoodItem

--- src/UI/FoodItem/FoodItem.js
+++ src/UI/FoodItem/FoodItem.js
@@ -1,17 +1,17 @@
 import React from "react";
 
-import classes from "./FoodItem.module.css"
+import classes from "./FoodItem.module.css";
 
-const FoodItem = props => {
-    return (
-        <div key={props.id} className={classes.FoodItem}>
-            <p>{props.name}</p>
-            <p>{props.link}</p>
-            <p>{props.favorite}</p>
-            <p>{props.rating}</p>
-            <p>{props.difficulty}</p>
-        </div>
-    )
-}
+const FoodItem = (props) => {
+  return (
+    <div key={props.id} className={classes.FoodItem}>
+      <p>{props.name}</p>
+      <p>{props.link}</p>
+      <p>{props.favorite}</p>
+      <p>{props.rating}</p>
+      <p>{props.difficulty}</p>
+    </div>
+  );
+};
 
 export default FoodItem;
