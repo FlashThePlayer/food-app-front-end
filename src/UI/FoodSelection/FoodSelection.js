baa42474fbccb131e93978c44f97b4cac608ee06
Timucin Acar <timucin.acar@cologne-intelligence.de>
2020-12-21 18:52:54 +0100

    enable FoodSelection to be Droppable

--- src/UI/FoodSelection/FoodSelection.js
+++ src/UI/FoodSelection/FoodSelection.js
@@ -3,59 +3,65 @@ import classes from "../../UI/FoodSelection/FoodSelection.module.css";
 import Spinner from "../Spinner/Spinner";
 import FoodItem from "../FoodItem/FoodItem";
 import Pagination from "../Pagination/Pagination";
-
+import { Droppable } from "react-beautiful-dnd";
 
 const FoodSelection = (props) => {
-    let foodList;
-    let pages;
+  let foodList;
+  let pages;
 
-    if (props.loading) {
-        foodList = (
-            <div className={classes.Spinner}>
-                <Spinner />
+  if (props.loading) {
+    foodList = (
+      <div className={classes.Spinner}>
+        <Spinner />
+      </div>
+    );
+  } else {
+    foodList = props.foods.map((food, index) => {
+      return (
+        <FoodItem
+          id={index}
+          name={food.name}
+          link={food.link}
+          favorite={food.favorite}
+          rating={food.rating}
+          difficulty={food.difficulty}
+        />
+      );
+    });
+    pages = (
+      <Pagination
+        currentPage={props.page}
+        maxPages={props.totalPages}
+        prevPage={props.prevPageHandler}
+        nextPage={props.nextPageHandler}
+      />
+    );
+  }
+
+  if (foodList.length === 0) {
+    foodList = (
+      <div className={classes.NoFoodBanner}>
+        <p>Create some food already!</p>
+      </div>
+    );
+    pages = null;
+  }
+
+  return (
+    <React.Fragment>
+      <div className={classes.Content}>
+        <Droppable droppableId={"droppableFood"}>
+          {(provided) => (
+            <div ref={provided.innerRef} {...provided.droppableProps}>
+              {foodList}
+              {provided.placeholder}
             </div>
-        );
-    } else {
-        foodList = props.foods.map((food, index) => {
-            return (
-                <FoodItem
-                    id={index}
-                    name={food.name}
-                    link={food.link}
-                    favorite={food.favorite}
-                    rating={food.rating}
-                    difficulty={food.difficulty}
-                />
-            );
-        });
-        pages = (
-            <Pagination
-                currentPage={props.page}
-                maxPages={props.totalPages}
-                prevPage={props.prevPageHandler}
-                nextPage={props.nextPageHandler}
-            />
-        );
-    }
-
-    if (foodList.length === 0) {
-        foodList = (
-            <div className={classes.NoFoodBanner}>
-                <p>Create some food already!</p>
-            </div>
-        );
-        pages = null;
-    }
-
-
-    return (
-        <React.Fragment>
-            <div className={classes.Content}>
-                {foodList}
-                {pages}
-            </div>
-        </React.Fragment>
-    )
-}
+          )}
+        </Droppable>
+        {pages}
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default FoodSelection;
