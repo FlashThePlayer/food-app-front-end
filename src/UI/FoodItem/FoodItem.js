71a0da66938bef13a45c2fd5d12a363c59e61d48
Timucin Acar <timucin.acar@cologne-intelligence.de>
2020-12-21 18:02:25 +0100

    enabled FoodItem to be draggable

--- src/UI/FoodItem/FoodItem.js
+++ src/UI/FoodItem/FoodItem.js
@@ -1,16 +1,31 @@
 import React from "react";
 
 import classes from "./FoodItem.module.css";
+import { Draggable } from "react-beautiful-dnd";
 
 const FoodItem = (props) => {
   return (
-    <div key={props.id} className={classes.FoodItem}>
-      <p>{props.name}</p>
-      <p>{props.link}</p>
-      <p>{props.favorite}</p>
-      <p>{props.rating}</p>
-      <p>{props.difficulty}</p>
-    </div>
+    <Draggable
+      draggableId={"foodItem-" + props.id}
+      index={props.id}
+      key={props.id}
+    >
+      {(provided) => (
+        <div
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          key={props.id}
+          className={classes.FoodItem}
+        >
+          <p>{props.name}</p>
+          <p>{props.link}</p>
+          <p>{props.favorite}</p>
+          <p>{props.rating}</p>
+          <p>{props.difficulty}</p>
+        </div>
+      )}
+    </Draggable>
   );
 };
 
