66bf1ac0e18538170ad9f6f4f82d451f228314e7
Timucin Acar <timucin.acar@cologne-intelligence.de>
2020-12-26 01:09:18 +0100

    remove unused import

--- src/GraphQl/ApolloClient.js
+++ src/GraphQl/ApolloClient.js
@@ -2,7 +2,6 @@ import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { setContext } from 'apollo-link-context';
 import { InMemoryCache } from 'apollo-cache-inmemory';
-import { useSelector } from "react-redux";
 
 
 const httpLink = createHttpLink({
