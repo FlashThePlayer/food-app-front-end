e9f4e4a9d0de34f8c1405e831b198db33f1d83f5
Timucin Acar <timucin.acar@cologne-intelligence.de>
2020-12-29 16:16:13 +0100

    make use of FoodItem size

--- src/UI/FoodSelection/FoodSelection.js
+++ src/UI/FoodSelection/FoodSelection.js
@@ -19,7 +19,7 @@ const FoodSelection = (props) => {
     foodList = props.foods.map((food, index) => {
       return (
         <FoodItem
-          small={false}
+          size={props.size}
           id={food._id}
           draggableId={"foodSelectionDraggable-" + food._id}
           index={index}
