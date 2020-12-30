8a743dcbce97ccacf5b0893a01cc0c1ef0d460f7
Timucin Acar <timucin.acar@cologne-intelligence.de>
2020-12-30 14:47:05 +0100

    change food name to link and alter styling
    changed the food name to a link containing its id which will later be used to show detailed information about the food, maybe even updating it but this needs to be decided later

--- src/UI/FoodItem/FoodItem.js
+++ src/UI/FoodItem/FoodItem.js
@@ -4,41 +4,50 @@ import classes from "./FoodItem.module.css";
 import { Draggable } from "react-beautiful-dnd";
 import Icon from "../Icon/Icon";
 import { ICONS } from "../Icon/IconConstants";
+import { SIZE } from "./SizeConstants";
+import { NavLink } from "react-router-dom";
 
 const FoodItem = (props) => {
+  const maxRating = 5; //ok for now be definitely NEEDS a better place, best case given down by props
   let content;
   let cssClass;
+  let rating = _fillRating(props.rating, maxRating);
 
   switch (props.size) {
-    case "small":
+    case SIZE.SMALL:
       cssClass = classes.SmallFoodItem;
       content = (
         <React.Fragment>
-          <p>{props.name}</p>
+          <NavLink exact to={`getFood/${props.id}`}>
+            <p>{props.name}</p>
+          </NavLink>
         </React.Fragment>
       );
       content = _wrapInDroppableComponent(props, cssClass, content);
       break;
-    case "mid":
+    case SIZE.MID:
       cssClass = classes.MidFoodItem;
       content = (
         <React.Fragment>
-          <p>{props.name}</p>
-          <p>{props.rating}</p>
+          <NavLink exact to={`getFood/${props.id}`}>
+            <p>{props.name}</p>
+          </NavLink>
+          <div>{rating}</div>
           <p>{props.difficulty}</p>
         </React.Fragment>
       );
       content = _wrapInDroppableComponent(props, cssClass, content);
       break;
-    case "full":
-    default:
-      cssClass = classes.MidFoodItem;
+    case SIZE.BIG:
+      cssClass = classes.BigFoodItem;
       content = (
         <React.Fragment>
-          <p>{props.name}</p>
+          <NavLink exact to={`getFood/${props.id}`}>
+            <p>{props.name}</p>
+          </NavLink>
           <p className={classes.Link}>{props.link}</p>
           <p>{props.favorite}</p>
-          <p>{props.rating}</p>
+          <div>{rating}</div>
           <p>{props.difficulty}</p>
         </React.Fragment>
       );
@@ -70,6 +79,19 @@ const _wrapInDroppableComponent = (props, cssClass, content) => {
       )}
     </Draggable>
   );
+};
+
+const _fillRating = (rating, maxRating) => {
+  const emptyStarsCount = maxRating - rating;
+  const result = [];
+  for (let i = 0; i < rating; i++) {
+    result.push(<Icon icon={ICONS.FullStar} />);
+  }
+
+  for (let i = 0; i < emptyStarsCount; i++) {
+    result.push(<Icon icon={ICONS.EmptyStar} />);
+  }
+  return result;
 };
 
 export default FoodItem;
