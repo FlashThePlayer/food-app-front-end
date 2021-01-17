0ae095c727459f2f6f4d5b57f2002977fbe5562f
Timucin Acar <timucin.acar@cologne-intelligence.de>
2021-01-17 23:50:27 +0100

    add delete functionality to food page
    now the use can delete food if he wishes to

--- src/Pages/GetFood/GetFood.js
+++ src/Pages/GetFood/GetFood.js
@@ -1,35 +1,65 @@
-import React, { useEffect } from "react";
-import { useLazyQuery } from "@apollo/client";
-import { getFoodSchema } from "../../GraphQl/Schema/Schema";
+import React, { useEffect, useState } from "react";
+import { useLazyQuery, useMutation } from "@apollo/client";
+import { getFoodSchema, deleteFoodSchema } from "../../GraphQl/Schema/Schema";
 import Spinner from "../../UI/Spinner/Spinner";
 import { useParams } from "react-router";
 import FoodItem from "../../UI/FoodItem/FoodItem";
 import { SIZE } from "../../UI/FoodItem/SizeConstants";
+import ConfirmModal from "../../UI/Modal/ConfirmModal/ConfirmModal";
 
-const GetFood = () => {
+const GetFood = (props) => {
   let content = <Spinner />;
   let { id: foodId } = useParams();
 
+  const [displayModal, setDisplayModal] = useState(false);
   const [getFood, { data }] = useLazyQuery(getFoodSchema);
+  const [deleteFood, { loading }] = useMutation(deleteFoodSchema);
 
   useEffect(() => {
     getFood({ variables: { id: foodId } });
   }, [foodId, getFood]);
 
+  const displayModalHandler = () => {
+    setDisplayModal((prevState) => !prevState);
+  };
+
+  const deleteFoodHandler = () => {
+    deleteFood({ variables: { id: foodId } })
+      .then((response) => props.history.push("/getFoods"))
+      .catch((e) => console.log(e));
+  };
+
+  let deleteModal = undefined;
+
+  if (displayModal) {
+    deleteModal = (
+      <ConfirmModal
+        show={displayModal}
+        confirmHandler={deleteFoodHandler}
+        abortHandler={displayModalHandler}
+        message={"Do you really want to delete this Recipe?"}
+      />
+    );
+  }
+
   if (data) {
     const food = data.getFood;
 
     content = (
-      <FoodItem
-        size={SIZE.FULL}
-        name={food.name}
-        link={food.link}
-        difficulty={food.difficulty}
-        keywords={food.keywords}
-        rating={food.rating}
-        pictureLink={food.pictureLink}
-        recipe={food.recipe}
-      />
+      <React.Fragment>
+        {deleteModal}
+        <FoodItem
+          size={SIZE.FULL}
+          name={food.name}
+          link={food.link}
+          difficulty={food.difficulty}
+          onClickHandler={displayModalHandler}
+          keywords={food.keywords}
+          rating={food.rating}
+          pictureLink={food.pictureLink}
+          recipe={food.recipe}
+        />
+      </React.Fragment>
     );
   }
 
