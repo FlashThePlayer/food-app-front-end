a61d5127242d5863a6c1b153300440bbec188ed8
Timucin Acar <timucin.acar@cologne-intelligence.de>
2021-05-17 14:30:52 +0200

    finally update home page text a bit with a tutorial for new users

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,10 +1,19 @@
 import React from "react";
 
 import classes from "./HomePage.module.css";
+import {ICONS} from "../../UI/Icon/IconConstants";
+import Icon from "../../UI/Icon/Icon";
 
 const HomePage = (props) => (
   <div className={classes.Homepage}>
-    <p>HELLO WORLD!</p>
+    <h1>Welcome to your Food App Week planer!</h1>
+    <p>Create your own meal plan for each coming week</p>
+    <p>with your favorite meals you collected from the internet</p>
+    <p>try it out!</p>
+    <p>Click on <b>create Food</b> to create or store your favorite foods from the internet</p>
+    <p>Click on <b>your Food</b> to browser all food items you created</p>
+    <p>And finally click on <b>your Week</b> to create your own meal plan for the coming weeks</p>
+    <p>all done by drag an drop through the <Icon icon={ICONS.ControlIndicator} /> symbol</p>
   </div>
 );
 export default HomePage;
